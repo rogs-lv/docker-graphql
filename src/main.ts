305a0369bc354a7322d7d29ec50c982a5b9cbb8f
rogs <oliverrosasg@gmail.com>
2023-02-21 17:33:24 -0600

    major nueva version totalmente nueva

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
 
   await app.listen(3000);
   
-  console.log('Aplicación corriendo en puerto :::: ', 3000 );
+  console.log('Aplicación corriendo/ejecutandose en puerto :::: ', 3000 );
 }
 bootstrap();
